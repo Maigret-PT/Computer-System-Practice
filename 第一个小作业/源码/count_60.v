e4c8b75fe2b3d482e23416a99a24ef58c562ba34
Sleepyhead1111 <106743758+Sleepyhead1111@users.noreply.github.com>
2022-11-02 23:17:14 +0800

    Add files via upload

--- 第一个小作业/源码/count_60.v
+++ 第一个小作业/源码/count_60.v
@@ -2,25 +2,77 @@ module count_60(
     input wire rst,
     input wire clk,
     input wire en,
-    output wire [3:0] count,
+    output wire[7:0] count,
     output wire co
 );
-    wire co10,co6;
-    
+    wire co10,co10to6,co6;
     count_10 u_count_10(
     	.rst   (rst   ),
         .clk   (clk   ),
         .en    (en    ),
-        .count (count ),
-        .co    (co    )
+        .count (count[3:0] ),
+        .co    (co10    )
     );
-    
+    assign co10to6 = co10 & en;
     count_6 u_count_6(
     	.rst   (rst   ),
         .clk   (clk   ),
-        .en    (en    ),
-        .count (count ),
-        .co    (co    )
+        .en    (co10to6    ),
+        .count (count[7:4] ),
+        .co    (co6    )
     );
-    
+    assign co = co6 & co10to6;
+endmodule
+
+
+
+module count_10(
+    input wire rst,
+    input wire clk,
+    input wire en,
+    output reg [3:0] count,
+    output wire co
+);
+    always @ (posedge clk) begin
+        if (rst) begin
+            count <= 4'b0;
+            //co <= 1'b0;
+        end
+        else if (en) begin
+            if (count == 4'd9) begin
+                count <= 4'b0;
+                //co = 1'b1;
+            end
+            else begin
+                count <= count + 1'b1;
+                //co <= 1'b0;
+            end
+        end
+    end
+    assign co = (count == 4'd9);
+endmodule
+
+
+
+module count_6(
+    input wire rst,
+    input wire clk,
+    input wire en,
+    output reg [3:0] count,
+    output wire co
+);
+    always @ (posedge clk) begin
+      if (rst) begin
+        count <= 4'b0;
+      end
+      else if (en) begin
+        if (count == 4'd5) begin
+          count <= 4'd0;
+        end
+        else begin
+          count <= count +1'b1;
+        end
+      end
+    end
+    assign co = (count == 4'd5);
 endmodule
